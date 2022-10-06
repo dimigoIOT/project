1f359dfdb2d013eaa4dfcd5ff2e01c34689a3d67
mokmokahr <jbmok.workspace@gmail.com>
2022-10-06 10:02:22 +0900

    CHORE: Change file location

--- flaskr.py
+++ flaskr.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index_home():
-    return render_template('index.html')
+def index_home(name=None):
+    return render_template('index.html', name=name)
 
 @app.route('/test')
 def index_test():
